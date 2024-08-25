1a4efcc35b7c426d011553b2e829ce292794bf4a
AnkitRaj1372 <ankitraj1372@gmail.com>
2024-08-25 11:16:07 +0530

    sudo removed

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update the package list and install prerequisites
-sudo apt-get update
-sudo apt-get install -y software-properties-common
+apt-get update
+apt-get install -y software-properties-common
 
 # Add the wkhtmltopdf repository and install it
-sudo apt-add-repository -y ppa:ubuntu-security-proposed/ppa
-sudo apt-get update
-sudo apt-get install -y wkhtmltopdf
+apt-add-repository -y ppa:ubuntu-security-proposed/ppa
+apt-get update
+apt-get install -y wkhtmltopdf
