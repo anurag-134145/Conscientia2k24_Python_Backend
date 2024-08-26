e27d6373ce0bf4b8408a7ac7351b4397040d089a
AnkitRaj1372 <ankitraj1372@gmail.com>
2024-08-26 06:51:45 +0530

    done changes

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import pdfkit
 app = Flask(__name__)
 
 # Configure pdfkit to use the installed wkhtmltopdf executable
-# PDFKIT_CONFIG = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')  # Adjust path as necessary
+PDFKIT_CONFIG = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')  # Adjust path as necessary
 
 @app.route('/generate-pdf-from-html', methods=['GET'])
 def generate_pdf_from_html():
@@ -12,7 +12,7 @@ def generate_pdf_from_html():
     pdf_filename = 'output.pdf'
 
     # Generate PDF from the HTML file
-    pdfkit.from_file(html_file_path, pdf_filename)
+    pdfkit.from_file(html_file_path, pdf_filename, configuration=PDFKIT_CONFIG)
 
     # Send the generated PDF file as a response
     return send_file(pdf_filename, as_attachment=True, download_name='downloaded.pdf')
